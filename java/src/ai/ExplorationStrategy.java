184bb040f1046eaa2a6a1c7be7f5d4dc5a39c76c
Weronika Zygis <115404427+werka-z@users.noreply.github.com>
2025-02-11 09:13:00 +0100

    Add working exploration config and initial ExplorationStrategy

--- java/src/ai/ExplorationStrategy.java
+++ java/src/ai/ExplorationStrategy.java
@@ -1,4 +1,15 @@
 package ai;
 
-public class ExplorationStrategy {
+import model.Coordinates;
+import java.util.Set;
+
+public abstract class ExplorationStrategy {
+    protected final int boardSize;
+
+    protected ExplorationStrategy(int boardSize) {
+        this.boardSize = boardSize;
+    }
+
+    public abstract Coordinates getExplorationShot(Set<Coordinates> shotsFired, double[][] stateActionValues);
+
 }
